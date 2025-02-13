48f97351a0930a898b79557d46e7a164484b2506
fabiofernandes-dev <fabiofernandes.zip@gmail.com>
2025-02-13 01:52:40 -0300

    Add project files.

---
+++ DetectCapital/Program.cs
@@ -0,0 +1,22 @@
+﻿bool IsCapital(string word)
+{
+    if (string.IsNullOrEmpty(word))
+        return false; 
+
+    if (word == word.ToUpper() || word == word.ToLower())
+        return true;
+
+    if (char.IsUpper(word[0]) && word.Substring(1) == word.Substring(1).ToLower())
+        return true;
+
+    return false;
+}
+
+string[] words = { "USA", "FlaG", "Google", "leetcode", "A","","b" };
+
+foreach (var word in words)
+{
+    Console.WriteLine($"'{word}' -> {IsCapital(word)}");
+}
+
+Console.ReadKey();
